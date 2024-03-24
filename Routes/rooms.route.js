a46b5313bd9ff436672546139b10469c2d1425cc
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-03-24 12:50:43 +0530

    added child in a room

--- Routes/rooms.route.js
+++ Routes/rooms.route.js
@@ -22,6 +22,37 @@ roomRouter.post('/add-room', async (req, res) => {
     }
 });
 
+// Route to add a child to a room
+roomRouter.post('/:roomId/children', async (req, res) => {
+    const { title, subTitle, price, link, longitude, latitude } = req.body;
+    const roomId = req.params.roomId;
+
+    try {
+        const room = await RoomModel.findById(roomId);
+        if (!room) {
+            return res.status(404).json({ error: 'Room not found' });
+        }
+
+        // Check if the child already exists in any other room
+        const childExists = await RoomModel.exists({
+            'children.title': title
+        });
+
+        if (childExists) {
+            return res.status(400).json({ error: `Child already exists in a parent room which id is ${childExists._id}` });
+        }
+
+        // Create new child
+        const newChild = { title, subTitle, price, link, latitude, longitude };
+        room.children.push(newChild);
+        await room.save();
+        res.status(201).json({ msg: `Child added in ${room.roomType} room at ${room.floorNumber}th floor`, room });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
 
 
 module.exports = { roomRouter };
